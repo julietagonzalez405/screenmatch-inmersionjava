7ada24151d6da5220c0b444190c5fe8074dccfc2
julieta405 <julietagonzalez405@gmail.com>
2024-05-06 18:19:54 -0300

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        //System.out.println("Película Matrix");
+
+//Declaración de Variables
+        int fechaDeLanzamiento = 1999;
+        double evaluacion = 4.5;
+        boolean incluidoPlanBasico = true;
+        String nombre = "Matrix";
+        String sinopsis = "La mejor película del fin del milenio";
+        double mediaEvaluacion = (4.5+4.8+3)/3;
+
+        System.out.println("Película: "+ nombre);
+        System.out.println("Fecha de Lanzamiento: " + fechaDeLanzamiento);
+        System.out.println("Evaluación: " + evaluacion);
+        System.out.println("Incluida en Plan Básico: " + incluidoPlanBasico);
+        System.out.println("Sinopsis: " + sinopsis);
+        System.out.println("Media de la evaluación de Matrix: " + mediaEvaluacion);
+    }
+}
